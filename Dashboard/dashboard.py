53b2fb26aa12877dce96d49f80403a559eb596a1
brianilham <tristanilham@gmail.com>
2024-10-04 23:45:59 +0700

    Update dashboard.py

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -2,12 +2,15 @@ import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import os
 
-# Load the dataframe.
+root_folder = dashboard.py
+print(root_folder)
+
 df = pd.read_csv("new_order_data.csv")
 df['dteday'] = pd.to_datetime(df['dteday'])
 
-# --- Styling ---
+
 st.set_page_config(page_title="Bike Sharing Dashboard", layout="wide")
 st.markdown(
     """
@@ -31,10 +34,10 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-# --- Dashboard ---
+
 st.title("Bike Sharing Dashboard")
 
-# --- Sidebar ---
+
 st.sidebar.title("Filters")
 weather_options = df['weathersit_y'].unique()
 selected_weather = st.sidebar.multiselect(
@@ -49,7 +52,7 @@ start_date, end_date = st.sidebar.date_input(
     "Date Range", [df['dteday'].min(), df['dteday'].max()]
 )
 
-# --- Filter Data ---
+
 filtered_df = df[
     df['weathersit_y'].isin(selected_weather)
     & df['cluster'].isin(selected_cluster)
@@ -57,14 +60,14 @@ filtered_df = df[
     & (df['dteday'] <= pd.to_datetime(end_date))
 ]
 
-# --- Correlation between temperature and bike rentals ---
+
 st.header("Temperature vs. Rentals")
 correlation = filtered_df['temp_y'].corr(filtered_df['cnt_y'])
 st.write(f"Correlation: {correlation:.2f}")
 plt.xlabel("Temperature")
 plt.ylabel("Count of Bike Rentals")
 
-# --- Scatter Plot ---
+
 fig, ax = plt.subplots(figsize=(8, 4))
 sns.regplot(
     x='temp_y',
@@ -78,7 +81,7 @@ plt.xlabel("Normalized Temperature", fontsize=12)
 plt.ylabel("Total Bike Rentals", fontsize=12)
 st.pyplot(fig)
 
-# --- Weather Type vs. Rentals ---
+
 st.header("Weather Type vs. Rentals")
 weather_rentals = filtered_df.groupby('weathersit_y')['cnt_y'].mean().reset_index()
 fig, ax = plt.subplots(figsize=(8, 4))
@@ -92,7 +95,7 @@ plt.xlabel(
 plt.ylabel("Average Bike Rentals", fontsize=12)
 st.pyplot(fig)
 
-# --- Cluster Visualization ---
+
 st.header("Cluster Visualization")
 fig, ax = plt.subplots(figsize=(8, 4))
 sns.scatterplot(
